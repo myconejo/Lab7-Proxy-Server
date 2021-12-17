35ce96e3a1130a6d9fb4a492f68cd1f8ef8b0c36
myconejo <96284664+myconejo@users.noreply.github.com>
2021-12-17 20:00:32 +0900

    Update nop-server.py

--- nop-server.py
+++ nop-server.py
@@ -6,6 +6,10 @@
 #
 # usage: nop-server.py <port>                
 #
+
+# For testing concurrency, client calls two servers: tiny and nop-server.
+# If conncurency works, then even though the nop server is not responding, proxy will be able to receive and send tiny's response to the client
+
 import socket
 import sys
 
@@ -14,6 +18,8 @@ serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(('', int(sys.argv[1])))
 serversocket.listen(5)
 
+# this server does not do anything, even the client requests from this server. 
+# use infinite while loop and do not respond. 
 while 1:
   channel, details = serversocket.accept()
   while 1:
